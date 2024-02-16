52648fb53e4df2fd001e29155bcb4ec1594b7860
avishmita <mandal.avishmita@gmail.com>
2024-02-15 19:33:05 -0500

    unit tests

--- arithmetic-service/src/index.js
+++ arithmetic-service/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { add } = require("./arithmetica");
 app.use(cors());
 const port = 3000;
 
@@ -11,7 +12,7 @@ app.get('/', (req, res) => {
 app.get('/add', (req, res) => {
     const num1 = parseFloat(req.query.num1) || 0;
     const num2 = parseFloat(req.query.num2) || 0;
-    const sum = num1 + num2;
+    const sum = add(num1, num2);
 
     res.json({result:sum});
 })
